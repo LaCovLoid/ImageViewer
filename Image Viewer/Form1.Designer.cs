033a2592b40dc653d9a9d7c14b8a3d615ac016a9
LaCovLoid <46214120+LaCovLoid@users.noreply.github.com>
2019-10-29 00:42:49 +0900

    Add files via upload

---
+++ Image Viewer/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Image_Viewer {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1525, 841);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 809);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "폴더 찾기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(0, 541);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 274);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label1.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(299, 816);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "폴더 경로";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1422, 809);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label2.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(396, 816);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "파일 이름";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Location = new System.Drawing.Point(-3, 809);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "목록 보이기";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label4.Location = new System.Drawing.Point(-3, 809);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(2275, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(1330, 809);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "상세정보";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label6.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(208, 816);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "새로고침";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label7.Location = new System.Drawing.Point(913, 475);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 104);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "<";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label8.Location = new System.Drawing.Point(886, 475);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 104);
+            this.label8.TabIndex = 11;
+            this.label8.Text = ">";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label9.Location = new System.Drawing.Point(595, 818);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "파일 용량";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1523, 837);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Image Viewer/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Image_Viewer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Form1_Resize(null, null);//위치 배정
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            folderBrowserDialog1.SelectedPath = null;
+            folderBrowserDialog1.ShowDialog();
+            //폴더 경로 가져옴
+            if (!string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath)) {
+                string Croot = @"C:\";
+                string foldername = folderBrowserDialog1.SelectedPath;
+                //string Hanyu = foldername.Split('\\')[0]; // 드라이브C루트를 전부 막음
+                if (foldername == Croot) {
+                    folderBrowserDialog1.SelectedPath = null;
+                    MessageBox.Show("C드라이브(시스템 드라이브) 루트는 선택할 수 없습니다.");
+                } else {
+                    listBox1.Items.Clear();
+                    label1.Text = foldername;
+                    pictureBox1.Image = null;
+                    pictureBox1.BackColor = Color.Transparent;
+                    string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);//파일들 가져옴
+                    for (int i = 0; i < files.Length; ++i) {//목록 가져옴
+                        string images = files[i];
+                        if (Path.GetExtension(images) == ".jpg" || 
+                            Path.GetExtension(images) == ".bmp" ||
+                            Path.GetExtension(images) == ".png" ||
+                            Path.GetExtension(images) == ".jpeg") {
+                            string result = Path.GetFileName(images);
+                            listBox1.Items.Add(result);
+                        }
+                    }
+                    System.Windows.Forms.MessageBox.Show("파일 안의 이미지는 " + listBox1.Items.Count + "개 입니다", "Message");
+                    listBox1.Visible = true;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {//삭제
+            string name = listBox1.GetItemText(listBox1.SelectedItem);
+            string Fullname = label1.Text + "\\" + name;
+            if (File.Exists(Fullname)) {
+                pictureBox1.Image = null;  //이미지 삭제시 이미지 존재유무
+                File.Delete(Fullname);
+                listBox1.Items.Clear();//리스트 초기화
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);//재설정
+                for (int i = 0; i < files.Length; ++i) {//목록 가져옴
+                    string images = files[i];
+                    if (Path.GetExtension(images) == ".jpg" ||
+                        Path.GetExtension(images) == ".bmp" ||
+                        Path.GetExtension(images) == ".png" ||
+                        Path.GetExtension(images) == ".jpeg") {
+                        string result = Path.GetFileName(images);
+                        listBox1.Items.Add(result);
+                    }
+                }
+                pictureBox1.BackColor = Color.Transparent;
+            } else MessageBox.Show("그런파일 없쟝.");//삭제실패
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {//리스트 선택시 발동돼는 함정카드
+            try {
+                pictureBox1.Image = null;
+                string name = listBox1.GetItemText(listBox1.SelectedItem);
+                string Fullname = label1.Text + "\\" + name;
+                label2.Text = name;
+                Image im = GetCopyImage(Fullname); //경로에서 이미지 추출
+
+                pictureBox1.Image = im;
+                pictureBox1.BackColor = Color.Black;
+                label1.Location = new Point(label3.Width + button1.Width + label6.Width, ClientSize.Height - label1.Height - 5);//리스트 선택마다 경로와 이름위치 재설정
+                label2.Location = new Point(ClientSize.Width / 2, ClientSize.Height - label1.Height - 5);
+                label9.Location = new Point(ClientSize.Width / 2 + label2.Width, ClientSize.Height - label1.Height - 5);
+                Int64 fileSize = new FileInfo(Fullname).Length;
+                Double Filesized = Math.Round((Double)fileSize / 1024 / 1024, 2);
+                label9.Text = Filesized + "MByte";
+            } catch (Exception) {
+                MessageBox.Show("이미지가 아닙니다.");
+            }
+        }
+        private Image GetCopyImage(string path) {//비트맵 이미지 가져옴
+            using (Image im = Image.FromFile(path)) {
+                Bitmap bm = new Bitmap(im);
+                return bm;
+
+            }
+
+        }
+        //보이나 안보이나 >>>>>>>>>>>
+        private void label3_Click(object sender, EventArgs e) {
+            if (listBox1.Visible == true) {
+                listBox1.Visible = false;
+            } else listBox1.Visible = true; 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+            if (button1.Visible == true) {
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                listBox1.Visible = false;
+                label5.Visible = false;
+                label6.Visible = false;
+                label7.Visible = false;
+                label8.Visible = false;
+                label9.Visible = false;
+            } else {
+                label1.Visible = true;
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+                button1.Visible = true;
+                button2.Visible = true;
+                label5.Visible = true;
+                label6.Visible = true;
+                label7.Visible = true;
+                label8.Visible = true;
+                label9.Visible = true;
+            }
+        }
+        //위치설정>>>>>>
+        private void Form1_Resize(object sender, System.EventArgs e) {
+            pictureBox1.Width = ClientSize.Width;
+            pictureBox1.Height = ClientSize.Height;
+            label3.Location = new Point(0, ClientSize.Height - label3.Height);
+            listBox1.Location = new Point(0, ClientSize.Height - label3.Height - listBox1.Height);
+            button1.Location = new Point(label3.Width, ClientSize.Height - label3.Height);
+            button2.Location = new Point(ClientSize.Width - button2.Width, ClientSize.Height - button2.Height);
+            label5.Location = new Point(ClientSize.Width - button2.Width - label5.Width, ClientSize.Height - button2.Height);
+            label4.Location = new Point(0, ClientSize.Height - label3.Height);
+            label1.Location = new Point(label3.Width + button1.Width + label6.Width, ClientSize.Height - label1.Height - 5);
+            label2.Location = new Point( ClientSize.Width/2 , ClientSize.Height - label1.Height - 5);
+            label6.Location = new Point(label3.Width + button1.Width, ClientSize.Height - label1.Height - 5);
+            label7.Location = new Point(20, ClientSize.Height / 2 - label7.Height/2);
+            label8.Location = new Point(ClientSize.Width - label8.Width - 20, ClientSize.Height / 2 - label8.Height/2);
+            label9.Location = new Point(ClientSize.Width / 2 + label2.Width, ClientSize.Height - label1.Height - 5);
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+        }
+
+        private void label5_Click(object sender, EventArgs e) {//상세정보(폼2)띄우기
+            Form2 detail = new Form2(label2.Text,label1.Text, Path.GetExtension(label1.Text+label2.Text));
+            if (pictureBox1.Image != null) {
+                detail.Show();
+            } else MessageBox.Show("이미지가 없습니다");
+        }
+
+        private void label6_Click(object sender, EventArgs e) {
+            if (label1.Text == "폴더 경로") {
+                MessageBox.Show("경로가 잡히지 않았습니다.");
+            } else {
+                listBox1.Items.Clear();//리스트 초기화
+                string[] files = Directory.GetFiles(label1.Text);//재설정
+                for (int i = 0; i < files.Length; ++i) {//목록 가져옴
+                    string images = files[i];
+                    label2.Text = "";
+                    if (Path.GetExtension(images) == ".jpg" ||
+                        Path.GetExtension(images) == ".bmp" ||
+                        Path.GetExtension(images) == ".png" ||
+                        Path.GetExtension(images) == ".jpeg") {
+                        string result = Path.GetFileName(images);
+                        listBox1.Items.Add(result);
+                    }
+                }
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e) {
+            if (listBox1.SelectedIndex == 0) {
+                MessageBox.Show("목록의 처음입니다."); 
+            } else listBox1.SelectedIndex = listBox1.SelectedIndex - 1;
+        }
+
+        private void label8_Click(object sender, EventArgs e) {
+            if (listBox1.SelectedIndex == listBox1.Items.Count-1) {
+                MessageBox.Show("목록의 끝입니다.");
+            } else listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+        }
+
+        private void label4_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
